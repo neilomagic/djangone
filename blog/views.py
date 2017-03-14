b59b8eeba5df69603c714ff2cafc7c4f05e31193
Nelson Kabanda <Kabanda.Lutakome@fao.org>
2017-03-14 15:12:15 +0300

    Moriah Django app, third commit

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+from django.utils import timezone
 
 
 def index(request):
-    return render(request, 'blog/post_list.html', {})
+    # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
 
 
