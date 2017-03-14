d24ae6d7935c6975c087cfc54b0bce1ab4a86b46
Nelson Kabanda <Kabanda.Lutakome@fao.org>
2017-03-14 18:35:52 +0300

    Moriah Django app, fith commit. involves create/edit posts

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 
 
@@ -9,9 +10,9 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
-            default=timezone.now)
+        default=timezone.now)
     published_date = models.DateTimeField(
-            blank=True, null=True)
+        blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -19,4 +20,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
