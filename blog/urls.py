abd18bbd8c70190320a4b1e84e4444abc869cba4
Nelson Kabanda <Kabanda.Lutakome@fao.org>
2017-03-14 16:35:51 +0300

    Moriah Django app, fourth commit

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # url(r'^$', views.index, name='index')
     url(r'^$', views.index, name='index'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
